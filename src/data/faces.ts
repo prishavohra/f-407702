391861f5b09c118e2e4813d9662f114af3e8db4d
Prisha Vohra <152265967+prishavohra@users.noreply.github.com>
2025-04-18 10:58:26 +0530

    Create faces.ts

---
+++ src/data/faces.ts
@@ -0,0 +1,83 @@
+const faces = [
+    {
+      id: "001",
+      name: "Person 001",
+      dateAdded: "2024-04-10",
+      lastSeen: "2025-04-18",
+      images: [
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/001/1.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/001/2.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/001/3.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/001/4.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/001/5.jpeg"
+      ]
+    },
+    {
+      id: "002",
+      name: "Person 002",
+      dateAdded: "2024-04-11",
+      lastSeen: "2025-04-18",
+      images: [
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/002/1.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/002/2.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/002/3.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/002/4.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/002/5.jpeg"
+      ]
+    },
+    {
+      id: "003",
+      name: "Person 003",
+      dateAdded: "2024-04-12",
+      lastSeen: "2025-04-18",
+      images: [
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/003/1.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/003/2.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/003/3.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/003/4.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/003/5.jpeg"
+      ]
+    },
+    {
+      id: "004",
+      name: "Person 004",
+      dateAdded: "2024-04-13",
+      lastSeen: "2025-04-18",
+      images: [
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/004/1.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/004/2.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/004/3.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/004/4.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/004/5.jpeg"
+      ]
+    },
+    {
+      id: "005",
+      name: "Person 005",
+      dateAdded: "2024-04-14",
+      lastSeen: "2025-04-18",
+      images: [
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/005/1.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/005/2.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/005/3.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/005/4.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/005/5.jpeg"
+      ]
+    },
+    {
+      id: "006",
+      name: "Person 006",
+      dateAdded: "2024-04-15",
+      lastSeen: "2025-04-18",
+      images: [
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/006/1.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/006/2.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/006/3.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/006/4.jpeg",
+        "https://known-faces-upload.s3.ap-south-1.amazonaws.com/006/5.jpeg"
+      ]
+    }
+  ];
+  
+  export default faces;
+  
